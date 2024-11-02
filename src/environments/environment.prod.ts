fce6a7bc283b8f7ae74982eb2bcde1f5b5961e6b
JUAN PABLO ARAGON <92612390+jparagon23@users.noreply.github.com>
2024-11-02 16:29:43 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'https://appointmentproyect-production-2765.up.railway.app/api/v1',
+  API_URL: 'https://appointmentproyect-develop.up.railway.app/api/v1',
 };
