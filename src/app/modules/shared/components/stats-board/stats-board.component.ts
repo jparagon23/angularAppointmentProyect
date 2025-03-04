b815d8f1e02be0e771471ba314220edccfc41e66
JUAN PABLO ARAGON <jparagon2021@gmail.com>
2025-03-04 18:20:33 +0000

    Grafica ok

--- src/app/modules/shared/components/stats-board/stats-board.component.ts
+++ src/app/modules/shared/components/stats-board/stats-board.component.ts
@@ -11,6 +11,7 @@ import {
 import { Chart, ChartType, registerables } from 'chart.js';
 import { UserMatchesStats } from 'src/app/models/events/UserMatchesStats.model';
 import 'chartjs-adapter-date-fns';
+import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-stats-board',
@@ -22,22 +23,23 @@ export class StatsBoardComponent implements OnInit, OnChanges, AfterViewInit {
   @ViewChild('matchHistoryChart') chartRef!: ElementRef<HTMLCanvasElement>;
 
   private chart!: Chart;
+  faQuestionCircle = faQuestionCircle;
 
   matchHistory = [
-    { date: '2024-01-10', rivalRating: 3.2, userRating: 3.4, won: true, rivalName: 'Juan Pablo', result: '6-0 6-0' },
-    { date: '2024-01-12', rivalRating: 3.6, userRating: 3.5, won: false, rivalName: 'Carlos', result: '4-6 3-6' },
-    { date: '2024-02-15', rivalRating: 3.9, userRating: 3.7, won: true, rivalName: 'Andrés', result: '7-5 6-3' },
-    { date: '2024-02-18', rivalRating: 4.1, userRating: 3.65, won: false, rivalName: 'Felipe', result: '3-6 2-6' },
-    { date: '2024-02-20', rivalRating: 3.5, userRating: 3.8, won: true, rivalName: 'David', result: '6-4 6-4' },
-    { date: '2024-02-22', rivalRating: 4.3, userRating: 3.53, won: false, rivalName: 'Sebastián', result: '5-7 4-6' },
-    { date: '2024-02-25', rivalRating: 3.8, userRating: 3.83, won: true, rivalName: 'Mateo', result: '6-2 6-3' },
-    { date: '2024-02-28', rivalRating: 3.9, userRating: 3.88, won: true, rivalName: 'Luis', result: '6-1 7-5' },
-    { date: '2024-03-01', rivalRating: 4.4, userRating: 3.72, won: false, rivalName: 'Miguel', result: '3-6 4-6' },
-    { date: '2024-03-03', rivalRating: 4.0, userRating: 3.76, won: false, rivalName: 'Fernando', result: '2-6 5-7' },
-    { date: '2024-03-06', rivalRating: 3.7, userRating: 3.81, won: true, rivalName: 'Jorge', result: '6-3 6-4' },
-    { date: '2024-03-08', rivalRating: 3.95, userRating: 3.79, won: true, rivalName: 'Raúl', result: '6-4 6-3' },
-    { date: '2024-03-10', rivalRating: 4.2, userRating: 3.68, won: false, rivalName: 'Tomás', result: '4-6 3-6' },
-    { date: '2024-03-12', rivalRating: 3.85, userRating: 3.84, won: true, rivalName: 'Nicolás', result: '7-6 6-3' },
+    { date: '2024-01-10', rivalRating: 3.2, userRating: 3.4, won: true, rivalName: 'Juan Pablo', result: '6-0, 6-0' },
+    { date: '2024-01-12', rivalRating: 3.6, userRating: 3.5, won: false, rivalName: 'Carlos', result: '4-6, 3-6' },
+    { date: '2024-02-15', rivalRating: 3.9, userRating: 3.7, won: true, rivalName: 'Andrés', result: '7-5, 6-3' },
+    { date: '2024-02-18', rivalRating: 4.1, userRating: 3.65, won: false, rivalName: 'Felipe', result: '3-6, 2-6' },
+    { date: '2024-02-20', rivalRating: 3.5, userRating: 3.8, won: true, rivalName: 'David', result: '6-4, 6-4' },
+    { date: '2024-02-22', rivalRating: 4.3, userRating: 3.53, won: false, rivalName: 'Sebastián', result: '5-7, 4-6' },
+    { date: '2024-02-25', rivalRating: 3.8, userRating: 3.83, won: true, rivalName: 'Mateo', result: '6-2, 6-3' },
+    { date: '2024-02-28', rivalRating: 3.9, userRating: 3.88, won: true, rivalName: 'Luis', result: '6-1, 7-5' },
+    { date: '2024-03-01', rivalRating: 4.4, userRating: 3.72, won: false, rivalName: 'Miguel', result: '3-6, 4-6' },
+    { date: '2024-03-03', rivalRating: 4.0, userRating: 3.76, won: false, rivalName: 'Fernando', result: '2-6, 5-7' },
+    { date: '2024-03-06', rivalRating: 3.7, userRating: 3.81, won: true, rivalName: 'Jorge', result: '6-3, 6-4' },
+    { date: '2024-03-08', rivalRating: 3.95, userRating: 3.79, won: true, rivalName: 'Raúl', result: '6-4, 6-3' },
+    { date: '2024-03-10', rivalRating: 4.2, userRating: 3.68, won: false, rivalName: 'Tomás', result: '4-6, 3-6' },
+    { date: '2024-03-12', rivalRating: 3.85, userRating: 3.84, won: true, rivalName: 'Nicolás', result: '7-6, 6-3' },
   ];
   
 
@@ -125,6 +127,9 @@ export class StatsBoardComponent implements OnInit, OnChanges, AfterViewInit {
             beginAtZero: false,
             suggestedMin: Math.min(...this.matchHistory.map(m => m.userRating), ...this.matchHistory.map(m => m.rivalRating)) - 0.5,
             suggestedMax: Math.max(...this.matchHistory.map(m => m.userRating), ...this.matchHistory.map(m => m.rivalRating)) + 0.5,
+            ticks: {
+              stepSize: 0.25,
+            },
           },
         },
         plugins: {
@@ -134,8 +139,8 @@ export class StatsBoardComponent implements OnInit, OnChanges, AfterViewInit {
               label: (tooltipItem: any) => {
                 const match = this.matchHistory[tooltipItem.dataIndex];
                 return match.won
-                  ? ` Victoria -  ${match.rivalName} (${match.result})` 
-                  : ` Derrota -  ${match.rivalName} ( ${match.result})`;
+                  ? ` Victoria -  ${match.rivalName}(${match.rivalRating}) ${match.result}` 
+                  : ` Derrota -  ${match.rivalName}(${match.rivalRating}) ${match.result}`;
               },
             },
           },
