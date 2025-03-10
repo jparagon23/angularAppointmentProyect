800b6e2334d52ab3662424985a8d0454bab08c5a
JUAN PABLO ARAGON <jparagon2021@gmail.com>
2025-03-10 14:01:37 +0000

    feat:loader-messages

--- src/app/modules/shared/components/loader/loader.component.ts
+++ src/app/modules/shared/components/loader/loader.component.ts
@@ -1,9 +1,30 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.component.html',
 })
-export class LoaderComponent {
-  @Input() message!: string;
+export class LoaderComponent implements OnInit, OnDestroy {
+  @Input() message: string = ''; // Mensaje fijo
+  @Input() messages: string[] = []; // Lista de mensajes rotativos
+
+  private index = 0;
+  private intervalId: any;
+
+  ngOnInit(): void {
+    if (this.messages.length > 0) {
+      this.message = this.messages[this.index];
+
+      this.intervalId = setInterval(() => {
+        this.index = (this.index + 1) % this.messages.length;
+        this.message = this.messages[this.index];
+      }, 5000); // Cambia cada 3 segundos
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 }
