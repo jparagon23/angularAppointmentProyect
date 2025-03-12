292b536db2ca47e55d8d4bcf1e77f108d42c5890
JUAN PABLO ARAGON <jparagon2021@gmail.com>
2025-03-12 18:29:30 +0000

    Change of firts request

--- src/app/modules/auth/layout/auth-layout/auth-layout.component.ts
+++ src/app/modules/auth/layout/auth-layout/auth-layout.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-auth-layout',
   templateUrl: './auth-layout.component.html'
 })
-export class AuthLayoutComponent {
+export class AuthLayoutComponent implements OnInit {
+  initialData: any;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.getInitialSignUpData().subscribe({
+      next: (data) => {
+        this.initialData = data;
+      },
+      error: () => {
+        console.error('❌ Error al obtener los datos iniciales');
+      },
+    });
+  }
 }
