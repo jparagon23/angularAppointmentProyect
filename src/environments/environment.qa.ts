ed5d51abd88f51e4e592a88f179f3cd4e4ae8d92
JUAN PABLO ARAGON <jparagon2021@gmail.com>
2025-05-30 16:23:22 -0500

    Update environment.qa.ts

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://appointmentproyect-production.up.railway.app/api/v1',
+  API_URL: 'https://appointmentproyect-production.up.railway.app/api/v1',
   // otros valores específicos del entorno QA
 };
